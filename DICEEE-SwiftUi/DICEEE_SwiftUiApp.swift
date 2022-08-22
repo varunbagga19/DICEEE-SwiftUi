9616da600ae9c07fb292fd11e3927f1db7debc4e
Varun Bagga <varunbagga19@gmail.com>
2022-08-22 23:06:14 +0530

    Initial Commit

---
+++ DICEEE-SwiftUi/DICEEE_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DICEEE_SwiftUiApp.swift
+//  DICEEE-SwiftUi
+//
+//  Created by Varun Bagga on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct DICEEE_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
